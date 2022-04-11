c8558922963654fc327327e6cf80dcafebf52b05
Ubuntu <joseph@massdynamics.com>
2022-04-11 15:40:40 +0000

    point out that there are no acceptance tests yet

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -4,4 +4,5 @@ set -e -x
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-python3 -B -m unittest ${current_dir}/../tests/acceptance/*_test.py
+#python3 -B -m unittest ${current_dir}/../tests/acceptance/*_test.py
+echo "No acceptance tests written. use unit tests."
